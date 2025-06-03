d0fa4baca56361712aa716f6d0899b6e3a3948eb
MarcosVinicius <marcusvinycius73@gmail.com>
2025-06-03 19:55:02 -0300

    Initial commit

---
+++ backend/src/main/java/com/marcos_vinicius14/questoesenem/br/Application.java
@@ -0,0 +1,13 @@
+package com.marcos_vinicius14.questoesenem.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marcos_vinicius14/questoesenem/br/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos_vinicius14.questoesenem.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
